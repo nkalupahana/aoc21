1e1e62929bc0cfb6cdc6ac580cedf8a2cb9c73da
nkalupahana <avishkank@gmail.com>
2021-12-06 23:45:17 -0600

    better solution for day7-1

--- day7/day7-1.py
+++ day7/day7-1.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-positions = []
 with open("day7.txt") as f:
     positions = np.array([int(x) for x in f.readline().strip().split(",")])
-
-print(int(np.sum(abs(positions - np.median(positions)))), int(np.median(positions)))
+    print(int(np.sum(abs(positions - np.median(positions)))), int(np.median(positions)))
